4c95802bfac116c9dd9b4302bc941ed8b6ecc6e4
rahul2227 <rahul2227.l115@gmail.com>
2020-08-01 16:52:55 +0530

    optimiztion of code a little

--- lib/UI/Home/HomePageBody.dart
+++ lib/UI/Home/HomePageBody.dart
@@ -5,12 +5,15 @@ import 'package:Planets/UI/Home/PlanetRow.dart';
 class HomePageBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new Column(
-      children: <Widget>[
-        new PlanetRow(planets[0]),
-        new PlanetRow(planets[1]),
-        new PlanetRow(planets[2]),
-      ],
+    return new Expanded(
+      child: new ListView.builder(
+        itemBuilder: (context, index) => new PlanetRow(planets[index]),
+        itemCount: planets.length,
+        padding: new EdgeInsets.symmetric(
+            vertical:
+                16.0), //We decided to set a margin of 16dp as top and bottom for each row. That makes 32 dp separation between rows, but the first item top margin and the last item bottom margin are only 16dp.
+        // itemExtent: 152.0, we dont need to use this parameter as Expanded will automatically adjust values for each screen.
+      ),
     );
   }
 }
